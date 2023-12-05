cebc9e200185364e8bc8bacf13eeaf18f45c2d57
danikova <kovacs.daniel.9509@gmail.com>
2023-12-05 14:05:19 +0100

    dynamic title font size

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -2,5 +2,11 @@ import { useFormData } from "@/lib/hooks";
 
 export function Title() {
   const [{ title }] = useFormData();
-  return title && <h3 className="text-[6rem] italic">{title}</h3>;
+  return (
+    title && (
+      <h3 className="sm:text-[min(8vw,10rem)] text-[15vmin] italic whitespace-nowrap">
+        {title}
+      </h3>
+    )
+  );
 }
