20f765d853788536e0dc5797ab62766359a6dac7
danikova <kovacs.daniel.9509@gmail.com>
2023-12-05 11:02:05 +0100

    new logo added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useMemo } from "react";
 import { Helmet } from "react-helmet";
 import { useFormData } from "./lib/hooks";
-import { FloatingMenu } from "./components/floating-menu";
 import { Toaster } from "./components/ui/toaster";
 import { MainContent } from "./components/main-content";
+import { FloatingMenu } from "./components/floating-menu";
 import { BackgroundImage } from "./components/background-image";
+import LogoSVG from '../assets/event-horizon-logo-min-rotated.svg?react';
+
 
 export default function App() {
   const [{ title: _title }] = useFormData();
@@ -19,6 +21,7 @@ export default function App() {
         <title>{title}</title>
       </Helmet>
       <div className="sm:h-[100lvh] sm:w-[100lvw] sm:rotate-0 sm:mt-0 h-[100svw] w-[100svh] rotate-[90deg] [transform-origin:bottom_left] [margin-top:-50svh]">
+        <LogoSVG className="fixed w-[100px] top-4 left-4 stroke-[hsl(var(--foreground))] opacity-10 hover:opacity-80 animate-[opacity] duration-300 select-none" />
         <BackgroundImage className="absolute top-0 left-0 h-[100%] w-[100%] overflow-hidden flex justify-center items-center opacity-40 z-[-1] blur-sm" />
         <MainContent className="h-[100%] w-[100%] flex flex-col gap-y-10 justify-center items-center" />
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   base: "/event-horizon/",
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
